8708939e2384111d0f793cac105502eb3c70b4f3
lumilio <logistic-division@lumilium.com>
2021-10-15 19:40:10 +0200

    modifica generale dopo consulenza con tutor

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,3 +1,4 @@
+
 const items = [
     'img/01.jpg',
     'img/02.jpg',
@@ -21,3 +22,47 @@ const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ]
+
+const box_Main = document.getElementById('box-mainpic');
+const box_Column = document.getElementById('box-column');
+
+let theBigPic = '';
+let theOptionPic = '';
+let active = 0;
+
+for (let i = 0; i < items.length; i++) {
+    theBigPic += 
+    `<dv class="big-photo">
+        <img src="${items[i]}" alt="">
+        <div id="didascalia">
+            <h3>"${title[i]}"</h3>
+            <p>"${text[i]}"</p> 
+        </div>
+    </dv>`
+    theOptionPic +=
+    `<div class="lil-photo">
+        <img src="${items[i]}" alt="">
+    </div>`  
+}
+
+box_Main.innerHTML = theBigPic;
+document.getElementsByClassName('big-photo')[active].classList.add('choice');
+
+
+
+// pulsanti
+
+/* const up = document.getElementById('swipe-up-button');
+up.addEventListener('click', function () {
+    document.getElementById("select1").style.backgroundColor="rgba(0, 0, 0, 0)";
+    document.getElementById("pic1").style.padding = "10px";
+})
+
+
+const down = document.getElementById('swipe-down-button');
+down.addEventListener('click', function () {})
+ */
+
+
+
+
